2ed347237398333971e39defb23224807af9f178
Ruslan Lukmanov <lkmrus@gmail.com>
2024-01-03 22:45:04 +0400

    Minor fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
     }
   },
   routeRules: {
-    '/**': { proxy: { to: process.env.BASE_API_URL } }
+    '/api/**': { proxy: { to: process.env.BASE_API_URL } }
   },
   security: {
     headers: {
