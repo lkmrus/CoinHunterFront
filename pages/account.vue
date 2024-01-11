e418dbfeeea2f565275501d0794af2299575ccb8
Ruslan Lukmanov <lkmrus@gmail.com>
2024-01-12 00:16:52 +0400

    minor fix

--- pages/account.vue
+++ pages/account.vue
@@ -61,7 +61,7 @@
             />
             <button-custom
                 :value="$t('account_add_telegram_button_name')"
-                @click="goToUrl(`https://t.me/CurrencyScanner3000Bot?start=${editableUser.login}`)"
+                @click="goToUrl(`https://t.me/CurrencyScanner3000Bot?start=${editableUser.login}_${editableUser.id}`)"
             />
         </div>
         <div class="account-page__subscription">
